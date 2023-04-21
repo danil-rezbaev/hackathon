3575b3d63b1c29953f87e0d8a0071c283f252a67
kd2ex <kountdown@mail.ru>
2023-04-22 00:56:42 +0300

    orders cards created

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,13 @@ import './App.css';
 import { Route, Routes } from 'react-router-dom'
 import Main from "./pages/main";
 import Error from "./components/Error"
+import Orders from './pages/order';
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path='/' element={<Main/>} />
+        <Route path='/' element={<Orders/>} />
         <Route path='*' element={<Error title="404" description="Страница не найдена"/>} />
       </Routes>
     </div>
